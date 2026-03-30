9fd34c1712c7d15a325fa80551a8087c2857767a
Alwyn206 <ad0273@srmist.edu.in>
2026-03-30 11:05:18 +0530

    Refactor to single file for uc2

--- UseCase2RoomInitialization.java
+++ UseCase2RoomInitialization.java
@@ -38,3 +38,50 @@ public class UseCase2RoomInitialization {
         System.out.println("-------------------------------------------");
     }
 }
+
+/**
+ * Abstract class representing a general Room concept.
+ */
+abstract class Room {
+    private String roomType;
+    private int numberOfBeds;
+    private double size;
+    private double price;
+
+    public Room(String roomType, int numberOfBeds, double size, double price) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() { return roomType; }
+    public int getNumberOfBeds() { return numberOfBeds; }
+    public double getSize() { return size; }
+    public double getPrice() { return price; }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 250.0, 100.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 400.0, 150.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 650.0, 250.0);
+    }
+}
